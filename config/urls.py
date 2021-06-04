866c311b853f0e44d9e82fbbdaba46c11aea8405
abhijitez <abhijit.nair659@gmail.com>
2021-06-05 04:17:48 +0530

    Graphql endpoint initialization for the django models

---
+++ config/urls.py
@@ -0,0 +1,27 @@
+"""config URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+"""
+Because you’re not using the Django template engine’s cross-site request forgery (CSRF) protection features, you’ll also need to import Django’s csrf_exempt decorator to mark the view as exempt from CSRF protection
+"""
+from django.views.decorators.csrf import csrf_exempt
+from graphene_django.views import GraphQLView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("graphql", csrf_exempt(GraphQLView.as_view(graphiql=True)))
+]
